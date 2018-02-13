1326d9fdecba623fc36ea21eb03b78e2bb9112b1
KHRASSS17 <36438460+KHRASSS17@users.noreply.github.com>
2018-02-13 10:06:09 +0100

    Add files via upload

---
+++ ProgramaCine.java
@@ -0,0 +1,15 @@
+/*	Fichero: ProgramaCine.Java
+	Autor: Christian Lara
+	Descripcion: Programa que nos ayuda a saber si ir al cine o no.
+*/
+// Inicio de la clase ProgramacionCine
+
+public class ProgramaCine
+{
+		//Metodo principa, que se llama al arrancar el programa
+		public static void main(String[] argumentos)
+        {
+                System.out.println ("Mi primer programa");
+		System.out.println ("PROGRAMA CINE");
+        }
+}
